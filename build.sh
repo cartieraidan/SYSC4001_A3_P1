ddcc28bc5860ad2ed7d41c8d4e9fdf41592dc0b8
cartieraidan <cartieraidan@gmail.com>
2025-12-01 02:10:45 -0500

    start implement for round robin

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ else
 fi
 
 g++ -std=c++17 -g -O0 -I . -o bin/interrupts_EP interrupts_EP_Aidan_Sultan.cpp
-#g++ -g -O0 -I . -o bin/interrupts_RR interrupts_student1_student2_RR.cpp
+g++ -std=c++17 -g -O0 -I . -o bin/interrupts_RR interrupts_RR_Aidan_Sultan.cpp
 #g++ -g -O0 -I . -o bin/interrupts_EP_RR interrupts_student1_student2_EP_RR.cpp
--- interrupts_RR_Aidan_Sultan.cpp
+++ interrupts_RR_Aidan_Sultan.cpp
@@ -1,11 +1,4 @@
-/**
- * @file interrupts.cpp
- * @author Sasisekhar Govind
- * @brief template main.cpp file for Assignment 3 Part 1 of SYSC4001
- * 
- */
-
-#include<interrupts_student1_student2.hpp>
+#include<interrupts_Aidan_Sultan.hpp>
 
 void FCFS(std::vector<PCB> &ready_queue) {
     std::sort( 
@@ -17,6 +10,46 @@ void FCFS(std::vector<PCB> &ready_queue) {
             );
 }
 
+void PID_Sort(std::vector<PCB> &ready_queue) {
+    std::sort(
+        ready_queue.begin(),
+        ready_queue.end(),
+        []( const PCB &first, const PCB &second ) {
+            return (first.PID < second.PID);
+        }
+    );
+}
+
+PCB& find_PCB(int PID, std::vector<PCB> &job_list) {
+    
+    for (auto &process : job_list) {
+        
+        if (process.PID == PID) {
+            return process;
+        }
+    }
+
+    //should always be found
+    throw std::runtime_error("PCB not found in job_list");
+
+}
+
+void remove_from_waitQ(int PID, std::vector<PCB> &wait_queue) {
+    
+    if (!(wait_queue.empty())) {
+        //lambda push process we want to the end and captures PID
+        std::stable_partition(
+            wait_queue.begin(),
+            wait_queue.end(),
+            [PID]( const PCB &p ) {
+                return p.PID != PID;
+            }
+        );
+
+        wait_queue.pop_back(); //removing from wait queue
+    }
+}
+
 std::tuple<std::string /* add std::string for bonus mark */ > run_simulation(std::vector<PCB> list_processes) {
 
     std::vector<PCB> ready_queue;   //The ready queue of processes
@@ -25,6 +58,9 @@ std::tuple<std::string /* add std::string for bonus mark */ > run_simulation(std
                                     //to the "Process, Arrival time, Burst time" table that you
                                     //see in questions. You don't need to use it, I put it here
                                     //to make the code easier :).
+
+    std::vector<PCB> allocate_queue; //The wait queue for a process if there are no partitions available
+    std::unordered_map<int, unsigned int> io_duration; //I/O duration map for PCB's in wait queue
 
     unsigned int current_time = 0;
     PCB running;
@@ -36,6 +72,8 @@ std::tuple<std::string /* add std::string for bonus mark */ > run_simulation(std
 
     //make the output table (the header row)
     execution_status = print_exec_header();
+
+    std::cout << "Starting while loop" << std::endl;
 
     //Loop while till there are no ready or waiting processes.
     //This is the main reason I have job_list, you don't have to use it.
@@ -49,9 +87,14 @@ std::tuple<std::string /* add std::string for bonus mark */ > run_simulation(std
         //Population of ready queue is given to you as an example.
         //Go through the list of proceeses
         for(auto &process : list_processes) {
+            bool assigned; //flag if process got allocated
+            bool arrived = false; //flag if the process arrived to be allocated
+            
             if(process.arrival_time == current_time) {//check if the AT = current time
                 //if so, assign memory and put the process into the ready queue
-                assign_memory(process);
+                assigned = assign_memory(process); 
+
+                arrived = true; 
 
                 process.state = READY;  //Set the process state to READY
                 ready_queue.push_back(process); //Add the process to the ready queue
@@ -59,18 +102,114 @@ std::tuple<std::string /* add std::string for bonus mark */ > run_simulation(std
 
                 execution_status += print_exec_status(current_time, process.PID, NEW, READY);
             }
+
+            //adds process to allocate queue if there are no free partitions at the time of arrival
+            if (arrived && !assigned) {
+                allocate_queue.push_back(process);
+            }
+        }
+
+        //try and allocate memory for process
+        if (!(allocate_queue.empty())) {
+
+            for (auto &process : allocate_queue) { 
+
+                if (assign_memory(process)) {
+                    process.state = READY;  //Set the process state to READY
+                    ready_queue.push_back(process); //Add the process to the ready queue
+                    job_list.push_back(process); //Add it to the list of processes
+
+                    execution_status += print_exec_status(current_time, process.PID, NEW, READY);
+
+                }
+            }
+
+            //removes from allocate queue
+            allocate_queue.erase(
+                std::remove_if(allocate_queue.begin(), allocate_queue.end(),
+                [](const PCB &p){ return p.state == READY; }),
+                allocate_queue.end()
+            );
         }
 
         ///////////////////////MANAGE WAIT QUEUE/////////////////////////
-        //This mainly involves keeping track of how long a process must remain in the ready queue
+        if (!(io_duration.empty())) { //decrement of I/O of process in wait queue
+            
+            for (auto pair = io_duration.begin(); pair != io_duration.end();) {
+                int PID = pair->first;
+                unsigned int &remaining = pair->second;
+                
+                //if process done I/O wait
+                if (remaining == 0) {
+                    PCB current_PCB = find_PCB(PID, job_list); //gets PCB referenxe
 
+                    current_PCB.state = READY;  //Set the process state to READY
+
+                    sync_queue(job_list, current_PCB);
+
+                    remove_from_waitQ(current_PCB.PID, wait_queue); //remove from wait queue
+                    ready_queue.push_back(current_PCB); //Add the process to the ready queue
+
+                    execution_status += print_exec_status(current_time, current_PCB.PID, WAITING, READY);
+
+                    pair = io_duration.erase(pair); //erase from map and return valid iterator
+
+                } else {
+                    --remaining; //decrement I/O duration time
+                    ++pair;
+
+                }
+
+            }
+
+        }
+
+        if (running.PID != -1) { //not doing logic on idle CPU
+            
+            running.remaining_time--; //decrements current running process
+
+            if (running.remaining_time == 0) { //if process is done
+
+                execution_status += print_exec_status(current_time, running.PID, RUNNING, TERMINATED);
+                
+                terminate_process(running, job_list);
+                idle_CPU(running); //no longer any process in CPU
+
+            } else if ((current_time - running.start_time) % (running.io_freq) == 0 && running.io_freq != 0) { //if process requires IO
+
+                wait_queue.push_back(running); //add to I/O wait queue
+                running.state = WAITING;
+
+                sync_queue(job_list, running); //don't know if needed
+                
+                io_duration[running.PID] = running.io_duration - 1; //adding to map to track I/O duration
+
+                execution_status += print_exec_status(current_time, running.PID, RUNNING, WAITING);
+
+                idle_CPU(running); //idle CPU to go back to PID -1
+
+            }
+        }
+        
         /////////////////////////////////////////////////////////////////
 
         //////////////////////////SCHEDULER//////////////////////////////
-        FCFS(ready_queue); //example of FCFS is shown here
+        PID_Sort(ready_queue); //tested and works, puts Highest PID to back of queue
+        
+        //schedule new process if CPU idle
+        if (running.PID == -1 && !(ready_queue.empty())) {
+            run_process(running, job_list, ready_queue, current_time);
+
+            execution_status += print_exec_status(current_time, running.PID, READY, RUNNING);
+        }
         /////////////////////////////////////////////////////////////////
 
+        current_time++;
+
     }
+
+    //currently stuck in loop
+    std::cout << "Ending while loop" << std::endl;
     
     //Close the output table
     execution_status += print_exec_footer();
